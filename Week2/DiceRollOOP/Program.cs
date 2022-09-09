a4f76f6dbbd6e86e7d503f390d066412a28f5841
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2022-09-09 19:31:27 +0700

    add POP vs OOP examples

---
+++ Week2/DiceRollOOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DiceRollOOP // Note: actual namespace depends on the project name.
+{
+    class Dice
+    {
+        private int number = 0;
+
+        public void Roll()
+        {
+            number = new Random().Next(1, 7);
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+    }
+
+    internal class DiceRollOOP
+    {
+        private static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            dice.Roll();
+            Console.WriteLine("Roll: " + dice.GetNumber());
+        }
+    }
+}
---
+++ Week2/DiceRollPOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DiceRollPOP // Note: actual namespace depends on the project name.
+{
+    internal class DiceRollPOP
+    {
+        static int Roll()
+        {
+            return new Random().Next(1, 7);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Roll());
+        }
+    }
+}
