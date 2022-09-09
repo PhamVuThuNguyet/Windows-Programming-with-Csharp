6a0f1f30d212373dcae3dc241f54fb1d7fb4e4a0
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2022-09-09 20:19:47 +0700

    Week 3

---
+++ Week3/Dice/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Dice
+    {
+        private int sidesNo;
+        private int number;
+
+        public void InputSidesNo()
+        {
+            Console.Write("Input number of dice sides: ");
+            int sides = int.Parse(Console.ReadLine());
+            SetSides(sides);
+        }
+
+        private void SetSides(int sides)
+        {
+            sidesNo = sides;
+        }
+
+        public int GetSides()
+        {
+            return sidesNo;
+        }
+
+        public void Roll()
+        {
+            int random = new Random().Next(1, GetSides());
+            SetRoll(random);
+        }
+
+        private void SetRoll(int _number)
+        {
+            number = _number;
+        }
+
+        public int GetRoll()
+        {
+            return number;
+        }
+    }
+
+    class Guessing
+    {
+        private bool isTrue;
+
+        private void SetStatus(bool status)
+        {
+            isTrue = status;
+        }
+
+        public void Guess(int rightNumber)
+        {
+            Console.Write("Enter your number: ");
+            int guessNumber = int.Parse(Console.ReadLine());
+            SetStatus(CheckNumber(guessNumber, rightNumber));
+        }
+
+        private bool CheckNumber(int num1, int num2)
+        {
+            return num1 == num2;
+        }
+
+        public bool GetStatus()
+        {
+            return isTrue;
+        }
+    }
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            dice.InputSidesNo();
+            dice.Roll();
+
+            Guessing guessing = new Guessing();
+            guessing.Guess(dice.GetRoll());
+            Console.WriteLine("Your guessing is "+ guessing.GetStatus());
+            Console.WriteLine("Right Number: "+ dice.GetRoll());
+        }
+    }
+}
