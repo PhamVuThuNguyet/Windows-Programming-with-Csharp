ce883be56f60ac15d85314d8b7ec23ca9de86f9e
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2022-10-17 19:12:25 +0700

    Complete Week 7 Exercises

---
+++ Week7/Events/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Events
+{
+    public class CustomEventArgs : EventArgs
+    {
+        public bool IsSuccessful { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class EventLogic
+    {
+        public delegate void CustomEventHandler(Object sender, CustomEventArgs args);
+
+        public event CustomEventHandler CustomEvent;
+
+        public void StartProcess()
+        {
+            var data = new CustomEventArgs();
+
+            try
+            {
+                Console.WriteLine("Process Started!");
+
+                Console.WriteLine("Input Event Name: ");
+
+                data.Name = Console.ReadLine();
+                data.IsSuccessful = true;
+
+                OnProcessCompleted(data);
+            }
+            catch (Exception ex)
+            {
+                data.IsSuccessful = false;
+                data.Name = "NULL";
+                OnProcessCompleted(data);
+            }
+        }
+
+        protected virtual void OnProcessCompleted(CustomEventArgs args)
+        {
+            CustomEvent?.Invoke(this, args);
+            Console.WriteLine("Event Name: " + args.Name);
+            Console.WriteLine("Process " + (args.IsSuccessful ? "Completed Successfully" : "failed"));
+        }
+    }
+
+
+    internal class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            EventLogic eventLogic = new EventLogic();
+            eventLogic.StartProcess();
+        }
+    }
+}
